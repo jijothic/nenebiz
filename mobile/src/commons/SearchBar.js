acbfc2865d7bcbbbc9138d96c185a4f34e3141d9
jijothic <george.jijothic@gmail.com>
2019-05-14 16:09:20 +0300

    Updated search bar

--- mobile/src/commons/SearchBar.js
+++ mobile/src/commons/SearchBar.js
@@ -4,8 +4,10 @@ import { View, Text, StyleSheet } from 'react-native';
 export default class SearchBar extends Component {
   render() {
     return (
-      <View>
-        <Text>Hello</Text>
+      <View style={styles.wrapper}>
+        <View style={styles.searchContainer}>
+          <Text>Search for a business/service...</Text>
+        </View>
       </View>
     );
   }
@@ -13,6 +15,11 @@ export default class SearchBar extends Component {
 
 const styles = StyleSheet.create({
   wrapper: {
-
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    backgroundColor: 'rgba(255,255,255,0.9'),
+    width: '100%',
+    height: 80,
   },
 });
