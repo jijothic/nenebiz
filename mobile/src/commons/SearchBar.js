46992f709a1d912ae8f597ce49b0e2e2856164d0
jijothic <george.jijothic@gmail.com>
2019-05-14 16:51:35 +0300

    Updated search button

--- mobile/src/commons/SearchBar.js
+++ mobile/src/commons/SearchBar.js
@@ -40,5 +40,9 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.8,
     shadowRadius: 10,
     borderRadius: 3,
+    height: 40,
+    marginTop: 25,
+    marginLeft: 20,
+    marginRight: 20,
   },
 });
