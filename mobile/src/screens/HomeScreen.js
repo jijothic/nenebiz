5e5922b4b6487e98fbf2afb33fd6cc32737b3222
jijothic <george.jijothic@gmail.com>
2019-05-14 17:01:18 +0300

    Updated search bar design

--- mobile/src/screens/HomeScreen.js
+++ mobile/src/screens/HomeScreen.js
@@ -65,7 +65,9 @@ class HomeScreen extends Component {
       <Box f={1}>
         <StatusBar barStyle="light-content" />
         <Box f={1}>
-        <SearchBar />
+          <Box h={90}>
+            <SearchBar />
+          </Box>
         </Box>
         <Box h={130} bg="white" w={1}>
           <DealCaroussel />
