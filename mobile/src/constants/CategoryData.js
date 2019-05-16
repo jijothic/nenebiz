6a5fd3a94adbbc4478b98c852e2f4ef8451e0b14
jijothic <george.jijothic@gmail.com>
2019-05-17 02:07:26 +0300

    Updated path images

--- mobile/src/constants/CategoryData.js
+++ mobile/src/constants/CategoryData.js
@@ -1,15 +1,15 @@
 const categoryList = [
   {
     name: 'Homes',
-    photo: require('../../assets/img/products/homesCategory.png'),
+    photo: require('../../assets/img/products/categories/homesCategory.png'),
   },
   {
     name: 'Experiences',
-    photo: require('../../assets/img/products/experiencesCategory.png'),
+    photo: require('../../assets/img/products/categories/experiencesCategory.png'),
   },
   {
     name: 'Restaurants',
-    photo: require('../../assets/img/products/restaurantsCategory.png'),
+    photo: require('../../assets/img/products/categories/restaurantsCategory.png'),
   }
 ];
 export default categoryList;
