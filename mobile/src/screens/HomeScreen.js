7ff40c67610109be6c6c6f3ffedc97ce3b53e8b6
jijothic <george.jijothic@gmail.com>
2019-05-21 16:34:09 +0300

    Added some categories in home screen

--- mobile/src/screens/HomeScreen.js
+++ mobile/src/screens/HomeScreen.js
@@ -52,7 +52,7 @@ class HomeScreen extends Component {
       style.borderLeftColor = theme.color.greyLighter;
     }
     return (
-      <Box w={NUM_COLUMNS} bg="white" h={120} style={style}>
+      <Box w={3 / NUM_COLUMNS} bg="white" h={120} style={style}>
         <CategoryCard {...item} />
       </Box>
     );
