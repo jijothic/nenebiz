a43d4aa9097f011b2bceb28a5999e35bbd453763
jijothic <george.jijothic@gmail.com>
2019-05-22 18:10:06 +0300

    Add horizontal scroll

--- mobile/src/components/Horizontalscrollitems.js
+++ mobile/src/components/Horizontalscrollitems.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 class Horizontalscrollitems extends Component {
   state = {}
   render() {
     return (
-      <Text>Hello</Text>
+      <ScrollView
+        horizontal={true}
+        decelerationRate={0}
+        snapToInterval={200}
+        showsHorizontalScrollIndicator={false}
+      >
+        <View style={styles.scrollContainer}>
+          <Text>Hello</Text>
+          <Text>Hello</Text>
+
+        </View>
+
+      </ScrollView>
     );
   }
 }
 
 export default Horizontalscrollitems;
+
+const styles = StyleSheet.create({
+  scrollContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 10,
+    width: '100%',
+  }
+})
