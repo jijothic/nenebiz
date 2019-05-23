11c58b201f9a0d9ba1192112aa4ed4b93dccba39
jijothic <george.jijothic@gmail.com>
2019-05-24 01:33:58 +0300

    Add Fav compponents

--- mobile/src/components/NoResults.js
+++ mobile/src/components/NoResults.js
@@ -6,9 +6,20 @@ export default class NoResults extends Component {
   render() {
     return (
       <View>
-
+        <Text style={styles.heading}>Favourites</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 30,
+    fontWeight: '600',
+    marginBottom: 40,
+    color: colors.black01,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+});
 
