bb4277f61ec9720c9af4bfaea504b44d759525ec
jijothic <george.jijothic@gmail.com>
2019-05-23 21:19:05 +0300

    Added Fav components

---
+++ mobile/src/components/NoResults.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View,Text,ScrollView, TouchableHighlight, StyleSheet } from 'react-native';
+import colors from '../constants/colors'
+
+export default class NoResults extends Component {
+  render() {
+    return (
+      <Text>Hello</Text>
+    );
+  }
+}
+
--- mobile/src/screens/FavoritesScreen.js
+++ mobile/src/screens/FavoritesScreen.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { StatusBar } from 'react-native';
 import { Box, Text } from 'react-native-design-utility';
 
+import NoResults from '../components/NoResults';
+
 class Favoritescreen extends Component {
   state = {};
   render() {
     return (
       <Box f={1} center>
         <StatusBar barStyle="light-content" />
-        <Text>FavoritesScreen</Text>
+        <NoResults />
       </Box>
     );
   }
