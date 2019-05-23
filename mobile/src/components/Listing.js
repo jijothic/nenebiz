e7fe53200a7302b300a8e7970dc6428e9a8f3523
jijothic <george.jijothic@gmail.com>
2019-05-23 03:58:06 +0300

    Add listings

--- mobile/src/components/Listing.js
+++ mobile/src/components/Listing.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesome } from '@expo/vector-icons';
 import {
