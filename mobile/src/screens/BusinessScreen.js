c22bf1e7a05554e9a68eaa4fe09c3b58d19f8dd4
jijothic <george.jijothic@gmail.com>
2019-05-25 17:37:38 +0300

    Add Business Screen

--- mobile/src/screens/BusinessScreen.js
+++ mobile/src/screens/BusinessScreen.js
@@ -5,16 +5,16 @@ import { Box, Text } from 'react-native-design-utility';
 import ProfileBtn from '../commons/ProfileBtn';
 
 class BusinessScreen extends Component {
-  static navigationOptions = {
-    title: 'My Business',
-  };
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('name', 'nenebiz'),
+  });
 
   state = {};
   render() {
     return (
       <Box f={1} center>
         <StatusBar barStyle="dark-content" />
-        <Text>Business Screen</Text>
+        <Text>Business  Screen</Text>
       </Box>
     );
   }
