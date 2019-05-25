10b7cc600b546a1913f1a75da44190ab7d0049b4
jijothic <george.jijothic@gmail.com>
2019-05-25 18:24:35 +0300

    Add Button

--- mobile/src/screens/BizScreen.js
+++ mobile/src/screens/BizScreen.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import { StatusBar } from 'react-native';
 import { Box, Text } from 'react-native-design-utility';
+import { EvilIcons } from '@expo/vector-icons';
+import Button from '../commons/Button';
 
 class BizScreen extends Component {
+  static navigationOptions = {
+    title: 'My Business'
+  }
   state = {};
   render() {
     return (
       <Box f={1} center>
         <StatusBar barStyle="dark-content" />
-        <Text>Biz Screen</Text>
+        <Box>
+          <Button>
+            <Text>Add Business</Text>
+          </Button>
+        </Box>
       </Box>
     );
   }
