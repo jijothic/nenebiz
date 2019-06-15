a6b08e8d9523412b73998ecb27496e6eda7e96c7
jijothic <george.jijothic@gmail.com>
2019-06-15 18:26:48 +0300

    Add Review Screen

---
+++ mobile/src/components/Review.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text, Image, ScrollView } from 'react-native';
+
+class Review extends Component {
+  state = {  }
+  render() {
+    return (
+      <View>
+        <Text>Review Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default Review;
